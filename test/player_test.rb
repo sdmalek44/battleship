9418749a14df80b0ae881728402c0529018c2ab6
Stephen Malek <malek.stephen44@gmail.com>
2018-05-21 19:14:53 -0600

    write player test WIP

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,9 +2,15 @@ require './test/test_helper'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-  def test_two_or_three
+  def test_it_exists
     player = Player.new
 
-    assert_equal 3, player.two_or_three(input)
+    assert_instance_of Player, player
+  end
+
+  def test_it_initializes_a_board
+    player = Player.new
+
+    assert_instance_of Board, player.board
   end
 end
